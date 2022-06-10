14969d66b4a1df9434f092830ce6837b4b2b9a4d
adityamaladi <adityamaladi84@gmail.com>
2022-06-10 13:54:08 +0800

    masukin config

--- inti.py
+++ inti.py
@@ -16,8 +16,14 @@ import cv2
 import os
 import time
 
-#lottie hiasan
+# [theme]
+# primaryColor="#F63366"
+# backgroundColor="#FFFFFF"
+# secondaryBackgroundColor="#F0F2F6"
+# textColor="#262730"
+# font="sans serif"
 
+#lottie hiasan
 def load_lottieurl(url: str):
     r = requests.get(url)
     if r.status_code != 200:
@@ -42,11 +48,16 @@ classNames = []
 myList = os.listdir(path)
 #print(myList)
 
-menu = ["HOME", "PRESENSI","DATA", "ABOUT"]  # menu
+menu = ["HOME", "PRESENSI","DATA"]  # menu
 choice = st.sidebar.selectbox("Menu", menu)  # sidebar menu
+# Object notation
 
-col1, col2, col3 = st.columns(3)  # columns
-cap = cv2.VideoCapture(0)  # capture video
+
+col1, col2, col3, col4 = st.columns(4)  # columns
+# cap = cv2.VideoCapture(0)  # capture video
+
+
+
 if choice == 'PRESENSI':
     st.markdown("<h2 style='text-align: center; color: black;'>AMBIL DAFTAR HADIR</h2>",
                 unsafe_allow_html=True)  # title
@@ -113,11 +124,12 @@ if choice == 'PRESENSI':
                 markAttendance(name)
             FRAME_WINDOW.image(img)
             cv2.waitKey(1)
+
     else:
         pass
 
+
 elif choice == 'DATA':
-    with col2:
         df = pd.read_csv('Attendance.csv')
         st.subheader("HASIL PRESENSI")
         df = pd.read_csv('Attendance.csv')
@@ -131,78 +143,85 @@ elif choice == 'DATA':
         now = datetime.now()
         tString = now.strftime('%d:%m:%Y')
         if st.download_button(
-            label="Download data as CSV",
+            label="Download Data Absensi ",
             data=csv,
             file_name=f'Presensi Kelas/{tString}',
-            mime='text/csv',
+            mime='csv',
         ):
-            st.balloons()
-
-
-        # def load_lottieurl(url: str):
-        #     r = requests.get(url)
-        #     if r.status_code != 200:
-        #         return None
-        #     return r.json()
-        #
-        # lottie_url_hello = "https://assets5.lottiefiles.com/packages/lf20_V9t630.json"
-        # lottie_url_download = "https://assets4.lottiefiles.com/private_files/lf30_t26law.json"
-        # lottie_hello = load_lottieurl(lottie_url_hello)
-        # lottie_download = load_lottieurl(lottie_url_download)
-        # if st.download_button:
-        #     with st_lottie_spinner(lottie_download, key="download"):
-        #         time.sleep(0)
-        #     st.balloons()
+            st.success('File Berhasil Didowload')
 
 
 elif choice == 'HOME':
     with st.container():
-            st.title("Presensi Otomatis")
+        lottie_url_loading = 'https://assets10.lottiefiles.com/private_files/lf30_esg1l8r1.json'
+        lottie_loading = load_lottieurl(lottie_url_loading)
+        with st_lottie_spinner(lottie_loading):
+            time.sleep(5)
+        # st.title("WELCOME MOTHERFUCKER")
+        opeindo = Image.open('Images/Vilfren.jpg')
+        st.image(opeindo, caption='Vilfrend Excalibur')
+
+        st.write('##')
+        st.write('##')
+        left_column, right_column = st.columns(2)
+        with left_column:
             st.subheader('PRESENSI')
-            lottie_url_attendance = 'https://assets7.lottiefiles.com/packages/lf20_cznnfmoz.json'
-            lottie_attendance = load_lottieurl(lottie_url_attendance)
-            # lottie_attendance
+            st.write(
+            """
+            - Didalam menu presensi anda bisa langsung mengklik "Mulai Presensi" Maka Mesin akan secara otomatis mengambil data wajah peserta didik.
+            - Sebelum melakukan presensi pastian perangkat yang anda gunakan terhubung dengan WEBCAM "CCTV".
+            - Ketika melakukan presensi peserta didik dianjurkan untung menghadap kamera WEBCAM "CCTV" Untuk memaksimalkan akurasi mesin.
+            """)
+            st.write('##')
+        lottie_url_attendance = 'https://assets8.lottiefiles.com/packages/lf20_HQ3RPY.json'
+        lottie_attendance = load_lottieurl(lottie_url_attendance)
+        # lottie_attendance
+        with right_column:
+            st.write('##')
             st_lottie(
                 lottie_attendance,
                 reverse=False,
                 loop = True,
-                width=100
+                width=250
             )
             st.write('##')
-
-            st.subheader('DATA')
             st.write('##')
-            lottie_url_list = 'https://assets6.lottiefiles.com/packages/lf20_hmnohyvb.json'
-            lottie_list = load_lottieurl(lottie_url_list)
+
+        with left_column:
+            st.subheader('DATA')
+            st.write(
+                """
+                - Pada Menu data Terdapat Visualisai data yang akan muncul setelah anda melakukan presensi.
+                - Pada Menu data anda juga bisa langsung mendowload hasil presensi dengan format CSV ke prangkat anda.
+                """)
+            st.write('##')
+        lottie_url_list = 'https://assets2.lottiefiles.com/packages/lf20_pjagkisd.json'
+        lottie_list = load_lottieurl(lottie_url_list)
+        with right_column:
+            st.write('##')
             st_lottie(
                 lottie_list,
                 reverse=False,
                 loop=True,
-                width=100
+                width=250
             )
 
-            st.subheader('TENTANG')
-            st.write('##')
-            lottie_url_team = 'https://assets1.lottiefiles.com/packages/lf20_VWOntT.json'
-            lottie_team = load_lottieurl(lottie_url_team)
-            st_lottie(
-                lottie_team,
-                reverse=False,
-                loop=True,
-                width=100
-            )
-
-            st.write(
-                """
-                - Pastikan Device yang anda gunakan sudah terkoneksi dengan CCTV melalui jaringan Wifi.
-                - Masuk ke menu NAV BAR di pojok kiri atas, kemudian pilih menu "PRESENSI".
-                - Untuk memulai presensi, tekan CHECKBOX "MULAI PRESENSI ". 
-                - Untuk melihat hasil presensi, pergi ke menu NAV BAR dan klik bagian "DATA".
-                - Untuk mendowload hasil presensi, klik "DOWLOAD SEBAGAI CSV".
-                """)
-            st.write('##')
-
-        # st.image("Images_Attendance/adit.jpg",
-        #          width=990, caption="MY wife")
-
-
+        # with left_column:
+        #     st.subheader('TENTANG')
+        #     st.write(
+        #         """
+        #         - Pada Menu Tentang terdapat dokumentasi tentang projek absensi wajah otomatis ini.
+        #         - Pada Menu Tentang anda bisa mengakses informasi terkait Kelompok Vilfriend Excalibur
+        #         - Pada Menu Tentang Terdapat Profile dari Creator projek ini
+        #         """)
+        #     st.write('##')
+        # lottie_url_team = 'https://assets1.lottiefiles.com/packages/lf20_VWOntT.json'
+        # lottie_team = load_lottieurl(lottie_url_team)
+        # with right_column:
+        #     # st.write('#')
+        #     st_lottie(
+        #         lottie_team,
+        #         reverse=False,
+        #         loop=True,
+        #         width=250
+        #         )
